923fb99a6141155d60495af6c9b0824f21981d39
GustavSoderberg <gustav.soderberg@outlook.com>
2022-04-01 20:07:45 +0200

    Initial Commit

---
+++ WarChallange/WarChallangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarChallangeApp.swift
+//  WarChallange
+//
+//  Created by Gustav Söderberg on 2022-02-15.
+//
+
+import SwiftUI
+
+@main
+struct WarChallangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
